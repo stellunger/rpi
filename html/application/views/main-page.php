7b493b11b31b42a5b4d5dd08e33868ccb25f4a2e
stellunger <iamstellunger@gmail.com>
2014-01-08 19:15:07 +0600

    beta final1

--- html/application/views/main-page.php
+++ html/application/views/main-page.php
@@ -13,12 +13,12 @@
 					<p>Разработка и производство технологического оборудования.</p>
 					<img src="images/2.png" alt="">
 				</div>
-				<a href="<?php echo base_url(); ?>montage"><div class="box1">
+				<a href="<?php echo base_url(); ?>montage/montage/base"><div class="box1">
 					<h1>Монтаж</h1>
 					<p>Строительно-монтажные работы любой сложности «под ключ».</p>
 					<img src="images/3.png" alt="">
 				</div>
-				<a href="<?php echo base_url(); ?>service"><div class="box1">
+				<a href="<?php echo base_url(); ?>service/service/base"><div class="box1">
 					<h1>Сервис</h1>
 					<p>Введение в эксплуатацию и сервисное обслуживание объектов инфраструктуры.</p>
 					<img src="images/4.png" alt="">
